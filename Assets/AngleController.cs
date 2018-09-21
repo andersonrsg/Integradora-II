fe1dbce332b6759319c0095b49f3da6e5de5d30e
Leonardo <leonardo.pavanatto@gmail.com>
2018-09-21 09:07:00 -0300

    VR-ready

---
+++ Assets/AngleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleController : MonoBehaviour {
+
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start () {
+        direction = transform.forward;
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Vector3.Angle(direction, transform.forward));
+	}
+}
